62358a55b0b127e4ad5f4d9732f85eff12a1fb80
Hyejin <hj_yeo@naver.com>
2021-07-28 18:42:48 +0200

    Modification: index - API, Product cards, Responsive web

--- js/index.js
+++ js/index.js
@@ -1,81 +1,52 @@
-/* Déclaration d'URL */
-const URL = "http://localhost:3000/api/teddies";
+/* Afficher les produits */
+(async function() {
+    const products = await getProducts()
+    for (product of products) {
+        createCards(product)
+    }
+})()
 
-/* Récupération des données - Cours*/
-fetch(url)
-    .then(function(res) {
-        if (res.ok) {
-        return res.json();
-        }
-    })
-    .then(function(value) {
-        console.log(value);
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
+/* Appeler les données de l'API du serveur */
+function getProducts() {
+    const URL = "http://localhost:3000/api/teddies"
+    return fetch(URL)
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(products) {
+            return products
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+}
 
+/* Créer des cartes produits */
+function createCards(product) {
+    const templateElement = document.getElementById("templateProducts")
+    const cloneElement = document.importNode(templateElement.content, true)
+    
+    cloneElement.getElementById("card__image").src = product.imageUrl
+    cloneElement.getElementById("card__name").textContent = product.name
+    cloneElement.getElementById("card__price").textContent = product.price / 100 + (` €`)
+    cloneElement.getElementById("card__link").href = "pages/produit.html?id=" + product._id
+    
+    document.getElementById("cards").appendChild(cloneElement)
+}
+
+
+/* Créer une carte supplementaire */
 
 //Use 'Modulo' for an additional card when the number of item is odd. 
 //let emptyCardCreator = numberOfItem % 2 ;
 //if emptyCardCreator = 1; Create empty card ;
 
 
-const evenOddCalculate = (itemCount) => {
-    let cardCreator = (itemCount % 2);
-    if (cardCreator == 1) {
-        console.log('Create a card');
-    }
-};
-evenOddCalculate(5);
+// const evenOddCalculate = (itemCount) => {
+//     let cardCreator = (itemCount % 2);
+//     if (cardCreator == 1) {
+//         console.log('Create a card');
+//     }
+// };
+// evenOddCalculate(5);
 
-
-
-/*
-    Automatic creation of product cards depending on the number of items from data base;
-    (Number of product in the data base = number of product card on this page.)
-    1. Receive data : Number of products 
-    2. Create product cards using "innerHTML" of JS
-    3. Each card should contain: image of product, name, description, price
-    <script>
-        Call of Products API - GET
-        Terms
-        function nameOfFuction(Parameter) {
-            return Parameter;
-        }
-        alert(nameOfFuction(Argument));
-        function createProductCard() {
-            i = 0;
-            let numberOfProducts = nameOfArray.length
-            while (i < numberOfProducts) {
-                innerHTML ;
-                i += 1;
-            }
-        }
-        createProductCard();
-
-
-          <!-- <div class="card col-lg-6 shadow p-2 m-0">
-                    <a href="pages/produit.html">
-                        <img class="card-img-top" src="images/Logo_Orinoco.webp" alt="">
-                        <div class="card-body">                        
-                            <h3 class="card-title">Ourson 1</h3>
-                            <p class="card-text">Detail of product</p>
-                        </div>
-                    </a>
-
-                    <button type="button" class="btn btn-primary">Add to cart</button>
-                </div>
-                <br><br> -->
-    </script>
-
-   
-    4. Click on the card : 
-    <script>
-        let productLink = document.querySelector(#productLink);
-        console.log(productLink.getAttribute('href'));
-        productLink.setAttribute('href', 'http://produit.html/idOfProduct);
-
-    </script>
-        
-*/

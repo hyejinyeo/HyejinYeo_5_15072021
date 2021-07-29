3bc459f67c7a23ba385e95f4f2515473543c8792
Hyejin <hj_yeo@naver.com>
2021-07-29 17:47:34 +0200

    Modification: Produit - fetch API, displayProductInfo, Responsive Web

--- js/produit.js
+++ js/produit.js
@@ -1,106 +1,45 @@
-//JS practice
-let maVariable = 'Hyejin';
-let myVariableNumber = 2;
+/* Afficher les données du produit */
+(async function() {
+    const productId = getProductId()
+    const productInfo = await getProductInfo(productId)
+    insertProductInfo(productInfo)
+})()
 
-console.log(maVariable + ', ' + myVariableNumber);
-console.log(`Je m'appelle ${maVariable}.`);
-console.log('Je m\'appelle Hyejin.');
+/* Obtenir l'ID du produit de l'URL*/
+function getProductId() {
+    return new URL(document.location).searchParams.get('id')
+}
 
+/* Appeler les données par ID de l'API du serveur  */
+function getProductInfo(productId) {
+    return fetch(`http://localhost:3000/api/teddies/${productId}`)
+        .then((responseHttp) => responseHttp.json())
+        .catch(function(error) {
+            let errorMessage = document.getElementById("products__description")
+            errorMessage.innerHTML = `
+                <div class="text-center">
+                    <i class="fas fa-exclamation-triangle products--error"></i>
+                    <h2 class="mb-4">Erreur du Serveur</h2>
+                    <p class="mb-5">Nous n'avons pas réussi à afficher nos produits.</p>
+                </div>
+            `
+            error()
+        })
+}
 
-//JS Boucles
-
-//Boucle - For
-    for ( let i = 0; i < 3; i += 1){
-    console.log('For iteration : ' + i);
+/* Insérer les données */
+function insertProductInfo(productInfo) {
+    document.getElementById('description__image').src = productInfo.imageUrl
+    document.getElementById('description__name').textContent = productInfo.name
+    document.getElementById('description__id').textContent = `Réf :  ` + productInfo._id
+    document.getElementById('description__description').textContent = productInfo.description
+    document.getElementById('description__price').textContent = (productInfo.price / 100).toLocaleString("fr-FR", {style:"currency", currency:"EUR"})
+    let colorOption = document.getElementById('description__option')
+    for (let i = 0; i < productInfo.colors.length; i++) {
+        let option = document.createElement("option");
+        option.innerText = productInfo.colors[i];
+        colorOption.appendChild(option);
     }
-    //For array
-    let tableau0 = ['HMT', 'Hyejin', 'Bongout', 'bingbong'];
-    for ( let i = 0; i < tableau0.length; i += 1 ){
-        console.log(tableau0[i]);
-    }
-    //For in - key of object
-    let objet = {
-        name : 'coucou',
-        price : 3900,
-        description : 'Nobert your bestie'
-    };
-    for ( let k in objet) {
-        console.log(k);
-    }
-    //For of - value of array
-    let tableau1 = ['red', 'pink', 'purple', 'beige'];
-    for ( let v of tableau1) {
-        console.log(v);
-    }
+}
 
-//Boucle - While
-    let j = 0;
-    let tableau2 = ['pommes', 'cerises', 'bananes'];
-    while ( j < tableau2.length ){
-        console.log(tableau2[j]);
-        j += 1;
-    }
-
-//Boucle - Do... While
-    let a = 0;
-    let tableau3 = ['frontend', 'backend', 'fullstack'];
-    do {
-        console.log(tableau3[a]);
-        a += 1;
-    } while ( a < tableau3.length)
-
-//Le DOM
-console.log(window);
-window.alert('Hello');
-console.log('Largeur d\'ecran : ' + window.innerWidth);
-
-console.log(document);
-console.log('Chemin de fichier : ' + document.documentURI);
-
-//Selection 
-    let title = document.getElementById('title');
-    title.innerText = 'Nouveau titre';
-    title.innerHTMK = '<em>Nouveau</em> titre';
-
-    let citation = document.getElementsByClassName('citation');
-    console.log(citation);
-
-    let paragraph = document.getElementsByTagName('p');
-    console.log(paragraph); //tableau
-    console.log(paragraph[1].innerText); //contenu
-
-    console.log(document.querySelector('#id')); //selection with CSS selector
-    console.log(document.querySelector('.class')); 
-    console.log(document.querySelectorAll('a')); 
-
-    let a = document.querySelector('a');
-    console.log(a.getAttribute('href'));
-    a.setAttribute('href', './pages/produit.html/id');
-
-//Events - On click, it adds new class 'gris' that changes the color 
-let cardClickEvent = document.querySelectorAll('.product--card');
-cardClickEvent.addEventListener('click', function() {
-    cardClickEvent.classList.add('gris');   //Add new class on selector
-});
-
-//Events - Toggle effect
-let cardClickEvent = document.querySelectorAll('.product--card');
-cardClickEvent.addEventListener('click', function() {
-    this.classList.toggle('gris');   //Switch the class on every click
-});
-
-    //Same function but different way of writing
-    let cardClickEvent = document.querySelectorAll('.product--card');
-    cardClickEvent.addEventListener('click', () => {
-        this.classList.toggle('gris');   //Switch the class on every click. 
-    });
-    //Better to use 'function()' with EventListener
-
-    //How to use on P5
-    let cardClickEvent = document.querySelectorAll('.product--card');
-    let productLink = document.querySelector('a');
-    for (productLink of cardClickEvent) {
-        cardClickEvent.addEventListener('click', function() {
-            //Change the URL_ID of link
-        });
-    }
+/* Ajouter au Panier */

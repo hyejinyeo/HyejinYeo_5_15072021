415472a649d108308b0eddcd2897114a23d7eb75
Hyejin <hj_yeo@naver.com>
2021-08-03 19:03:17 +0200

    Modification: Produit - Réorganisation

--- js/produit.js
+++ js/produit.js
@@ -1,4 +1,17 @@
-/* Afficher les données du produit */
+/********************* GLOBAL SCOPE *********************/
+const productImage = document.getElementById('description__image');
+const productName = document.getElementById('description__name');
+const productIdentification = document.getElementById('description__id');
+const productDescription = document.getElementById('description__description');
+const productPrice = document.getElementById('description__price');
+const productOption = document.getElementById('description__option');
+const productQuantity = document.getElementById('description__quantity');
+
+
+
+/********************* AFFICHER LES DONNEES D'UN PRODUIT CHOISI *********************/
+
+/* Afficher les données d'un produit */
 (async function() {
     const productId = getProductId()
     const productInfo = await getProductInfo(productId)
@@ -20,24 +33,86 @@ function getProductInfo(productId) {
         })
 }
 
-/* Modifier breadcrumb - Nom d'ourson */
+/* Modifier le breadcrumb - Nom d'ourson */
 function changeBreadcrumb(productInfo) {
     document.getElementById('breadcrumb-name').textContent = `OURSON :  ` + productInfo.name
 }
 
 /* Insérer les données */
 function insertProductInfo(productInfo) {
-    document.getElementById('description__image').src = productInfo.imageUrl
-    document.getElementById('description__name').textContent = productInfo.name
-    document.getElementById('description__id').textContent = `Réf :  ` + productInfo._id
-    document.getElementById('description__description').textContent = productInfo.description
-    document.getElementById('description__price').textContent = (productInfo.price / 100).toLocaleString("fr-FR", {style:"currency", currency:"EUR"})
-    let colorOption = document.getElementById('description__option')
+    productImage.src = productInfo.imageUrl
+    productName.textContent = productInfo.name
+    productIdentification.textContent = `Réf :  ` + productInfo._id
+    productDescription.textContent = productInfo.description
+    productPrice.textContent = (productInfo.price / 100).toLocaleString("fr-FR", {style:"currency", currency:"EUR"})
     for (let i = 0; i < productInfo.colors.length; i++) {
         let option = document.createElement("option");
         option.innerText = productInfo.colors[i];
-        colorOption.appendChild(option);
+        productOption.appendChild(option);
     }
 }
 
+
+
+/********************* INCREMENTER LE NOMBRE D'ARTICLE DANS LE PANIER - NAV ICONE *********************/
+//Number of article
+//function itself to be in global.js
+
+
+
+/********************* AJOUTER AU PANIER *********************/
+
+
 /* Ajouter au Panier */
+let cart = document.querySelector(".card__button")
+cart.addEventListener('click', () => {
+    modifyCartNumbers()
+    //addToCart()
+})
+
+/* Afficher le nombre d'article ajouté dans le panier */
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('modifyCartNumbers')
+    if (productNumbers) {
+        document.querySelector('.nav-link span').textContent = productNumbers
+    }
+}
+
+/* Increment + 1 local storage au chaque clique sur le bouton "AJOUTER AU PANIER" */
+function modifyCartNumbers() {
+    let productNumbers = localStorage.getItem('modifyCartNumbers')
+    productNumbers = parseInt(productNumbers) // String -> Number
+    if (productNumbers) {
+        localStorage.setItem('modifyCartNumbers', productNumbers + 1)
+        document.querySelector('.nav-link span').textContent = productNumbers + 1
+    } else {
+        localStorage.setItem('modifyCartNumbers', 1)
+        document.querySelector('.nav-link span').textContent = 1
+    }
+    
+}
+
+onLoadCartNumbers()
+
+
+//03/08 Trial
+//Create an object 
+
+/*
+// function addToCart() {
+//     let id = getProductId();
+//     let productAdded = {
+//         name: productName.innerHTML,
+//         _id: id,
+//         price: parseFloat(productPrice.innerHTML),
+//         option: parseFloat(productOption.),
+//         quantity: parseFloat(product),
+//     };
+// }
+
+function addToLocalStorage() {
+    let arrayProductInCart = [];
+
+}
+
+*/

4f5f369c9e1f890d10b98622e19df9a6e86b34a5
Hyejin <hj_yeo@naver.com>
2021-08-06 18:06:35 +0200

    Bouton supprimer a/ function deleteProduct

--- js/panier.js
+++ js/panier.js
@@ -16,8 +16,9 @@ let cartTableBody = document.getElementById('cart-tableBody');
 let cartTotalPrice = document.getElementById('cart-totalPrice');
 
 
-displayProductsInCart();
+displayProductsInCart(); 
 displayTotalPrice();
+
 
 /* Afficher un résumé des produits dans le panier */
 function displayProductsInCart() {
@@ -29,18 +30,22 @@ function displayProductsInCart() {
             </td>
             <td>
                 <span class="h6">${productInCart[i].name}</span><br>
-                <span class="small">${productInCart[i].id}</span></td>
+                <span class="small" id="product-id">${productInCart[i].id}</span></td>
             <td>${productInCart[i].price.toLocaleString("fr-FR", {style:"currency", currency:"EUR"})}</td>
-            <td>${productInCart[i].option}</td>
+            <td id="product-option">${productInCart[i].option}</td>
             <td>
-                <i class="far fa-minus-square"></i>
+                <i class="far fa-minus-square" id="quantityMinus"></i>
                 <span>${productInCart[i].quantity}</span>
-                <i class="far fa-plus-square"></i>
-                <i class="far fa-trash-alt" id="delete"></i>
+                <i class="far fa-plus-square" id="quantityPlus"></i>&nbsp
+                <i class="far fa-trash-alt" id="quantityDelete"></i>
             </td>    
         </tr>
         `
-    }  
+    }
+    // Boutons
+    reduceQuantity();
+    increaseQuantity();
+    deleteProduct();
 }
 
 function displayTotalPrice() {
@@ -50,34 +55,81 @@ function displayTotalPrice() {
 }
 
 
-// let deleteProduct = document.getElementById('delete');
-
-// deleteProduct.forEach( function(deleteProduct) {
-//     deleteProduct.addEventListener('click', function() {
-//         const dataset = this.dataset;
-//         const 
-//     })
-// });
 
 
 
-/* 
-let removeCartItemButtons = document.getElementById('delete')
-for (let i=0; i < removeCartItemButtons.length; i++) {
-    let button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event)) {
-        let buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
+// Buttons inside Quantity 
+
+function reduceQuantity() {
+    //let quantityMinus = document.getElementById('quantityMinus');
+    //quantityMinus.addEventListener('click', () => {
+    //    alert('You clicked on minus button')
+    //})
+    // if remaining quantity is 1, entire line should be deleted
+    // if remaining quantitiy is > 2, return remaining quantity -1
+
+
+    // let quantityMinus = document.getElementById('quantityMinus');
+    // quantityMinus.addEventListener('click', () => {
+    //     let product = productInCart.find(
+    //         (obj) => obj.id === inputIdenfication && obj.option === inputColor
+    //         if (product) {
+    //             product.quantity += parseInt(inputQuantity);
+    //         } else {}
+    //     )
+    // });
+}
+
+function increaseQuantity() {
+    //let quantityPlus = document.getElementById('quantityPlus');
+    //quantityPlus.addEventListener('click', () => {
+    //    alert('You clicked on plus button')
+    //})
+    // increase 1 quantity of product on each click
+}
+
+
+function deleteProduct(id, option) {
+    //for (let i = 0; i < productInCart.length; i++) {
+    //   if (productInCart[i].id === id && productInCart[i].option === option)  {
+    //        productInCart.splice(i, 1)
+    //    }
+    //}
+}
+
+
+//////////////////////// Part I was working on - temporary breakpoint
+
+function deleteProduct() {
+    let quantityDelete = document.querySelectorAll('#quantityDelete');
+    for (let d = 0; d < quantityDelete.length; d++) {
+        quantityDelete[d].addEventListener('click', (event) => {
+            event.preventDefault();
+            let idOfProductToDelete = productInCart[d].id;
+            let optionOfProductToDelete = productInCart[d].option;
+            let quantityOfProductToDelete = productInCart[d].quantity;
+            let priceOfProductToDelete = productInCart[d].price;
+            
+            // Filtre des items afin de laisser des produits differents
+            productInCart = productInCart.filter( (el) => el.id !== idOfProductToDelete || el.option !== optionOfProductToDelete );
+            localStorage.setItem('Cart', JSON.stringify(productInCart));
+            
+            // Modifier 'QuantityInCart'
+            let productNumbers = localStorage.getItem('QuantityInCart');
+            productNumbers = parseInt(productNumbers); // String -> Number
+            localStorage.setItem('QuantityInCart', productNumbers - quantityOfProductToDelete);
+            
+            // Modifier 'TotalPrice'
+            let cartPrice = localStorage.getItem('TotalPrice');
+            cartPrice = parseInt(cartPrice);
+            localStorage.setItem('TotalPrice', cartPrice - priceOfProductToDelete);
+            
+            // Rechargement de la page
+            window.location.reload();
+        })
     }
 }
-
-function updateCartTotal() {
-
-}
-
-*/
-
+   
 
 
 /********************* FORMULAIRE *********************/

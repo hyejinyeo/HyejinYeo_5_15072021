64a0e64515faa98d71f7ea81d3d8a338c43444d4
Hyejin <hj_yeo@naver.com>
2021-08-17 10:58:54 +0200

    Ajout: Affichage du prix total

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,10 +1,20 @@
-/********************* Afficher un orderId (référence de la commande) *********************/
-let receivedOrderId = localStorage.getItem('orderId');
+/********************* Afficher l'orderId & le prix total  *********************/
 
+/* Afficher l'orderId */
+let receivedOrderId = localStorage.getItem('orderId');
 displayOrderId();
 function displayOrderId() {
     document.getElementById('orderid').innerText = receivedOrderId;
 }
+
+/* Afficher le prix total */
+displayTotalPrice()
+function displayTotalPrice() {
+    let totalPriceInCart = localStorage.getItem('TotalPrice');
+    totalPriceInCart = JSON.parse(totalPriceInCart);
+    document.getElementById('totalprice').innerHTML = totalPriceInCart.toLocaleString("fr-FR", {style:"currency", currency:"EUR"});
+}
+
 
 /********************* Vider local storage *********************/
 localStorage.clear();
